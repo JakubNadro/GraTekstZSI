59ed0b60eea7d662a0efac44c1935506b642e1a7
PW <53907986+wps0@users.noreply.github.com>
2020-10-03 12:05:23 +0200

    work in proggress

--- battlesystem.py
+++ battlesystem.py
@@ -1,5 +1,6 @@
 import random as r
 
+
 class Weapon:
     name = "Broń"
     damage = 0
@@ -11,12 +12,12 @@ class Weapon:
         self.luck = luck
 
 
-class MonsterAttack:
-    damage_modifier = 0
-
-    def __init__(self, dmg_mod):
-        self.damage_modifier = dmg_mod
-
+# class MonsterAttack:
+#     damage_modifier = 0
+#
+#     def __init__(self, dmg_mod):
+#         self.damage_modifier = dmg_mod
+#
 
 class Monster:
     defense = 0
@@ -25,10 +26,11 @@ class Monster:
     exp = 10
     name = "Potwór"
     entry_msg = "Spotykasz potwora " + name
+    run_msg = "Próbujesz uciec z walkis od potwora " + name
     lose_msg = "Przegrywasz z potworem " + name
     win_msg = "Wygrywasz z potworem " + name
 
-    def __init__(self, attack, defense, dmg, hp, exp, name, entry_msg, lose_msg, win_msg):
+    def __init__(self, attack, defense, dmg, hp, exp, name, entry_msg, lose_msg, win_msg, run_msg):
         self.attack = attack
         self.defense = defense
         self.damage = dmg
@@ -38,6 +40,7 @@ class Monster:
         self.entry_msg = entry_msg
         self.lose_msg = lose_msg
         self.win_msg = win_msg
+        self.run_msg = run_msg
 
 
 class Player:
---
+++ game.py
@@ -0,0 +1,26 @@
+from battlesystem import *
+
+
+class Location:
+    id = 0
+    name = "Bagna"
+    go_msg = "Możesz iść do lokalizacji o ID " + str(id)
+    monster = None
+    entry_msg = "Jesteś w " + name
+    id_dst = []
+
+    def __init__(self, id, name, go_msg, monster: Monster, entry_msg, id_dst):
+        self.id = id
+        self.name = name
+        self.go_msg = go_msg
+        self.monster = monster
+        self.entry_msg = entry_msg
+        self.id_dst = id_dst
+
+
+class GameManager:
+    monsters = []
+
+    def start(self):
+        print("Rozmoczynam grę...")
+        # STARTUJEMY Z SYSTEMU TOMASZA DO PORUSZANIA
