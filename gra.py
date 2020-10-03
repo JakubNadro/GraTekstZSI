7e7fa72e9cf703c6ed4571497486afaa2fd9dd20
donpecz111 <70844255+donpecz111@users.noreply.github.com>
2020-10-03 14:44:15 +0200

    Add files via upload

--- gra.py
+++ gra.py
@@ -1,19 +1,67 @@
-from gra.game import Location
+Monster(defense, dmg, hp, exp, name, entry_msg, lose_msg, win_msg, run_msg)
+Monster(0.3,
+        20 ,
+        80 ,
+        34 ,
+        "Drzewiec",
+        "Będąc zmęczonym siadasz na leżącym drzewie  i wbijasz w nie miecz . Czujesz że twoja prowizoryczne siedzenie zaczyna się podnosić. Odskakujesz od niego jak oparzony by ujrzeć starego drzewca ",
+        "Nie udało ci się pokonać rozgniewanego drzewca ten jednak postanowił zostawić twój osąd naturze po czym znika zostawiając ciebie rannego . Będąc na skraju przytomności widzisz zbliżające się do ciebie wilki i niedźwiedzie ",
+        "Udaje ci się wywalczyć  wolność u strażnika lasu .",
+        "Próbujesz przekonać go że jest  ci przykro ,.drzewiec wierzy ci i wypuszcza cię wolno ale pozostawia ci blizną na plecach.")
+Monster(0.9,
+        35 ,
+        60 ,
+        12,
+        "Minotaur",
+        "Czujesz odgłos kroków z korytarza przed tobą wychodzi humanoidalna postać z głową byka oraz wściekłymi czerwonymi oczami. trzyma on topór którym zaczyna wymachiwać  w każdą stronę w celu zastraszenia",
+        "Walka była długim pokazem siły między wami i żaden nie chciał się poddać lecz po długiej walce nie miałeś już sił więc potwór zadał ci szybką i bez bolesną śmierć na znak szacunku.",
+        "Walka była długim pokazem siły między wami i żaden nie chciał się poddać lecz po długiej walce potwór w końcu padł.",
+        ("Podczas próby ucieczki minotaur cię dogonił i zdjął ci głowę z ramion ", ))
 
-locations = [
-    Location(0, "Start gry", " ", None, "Witaj! Możesz przejść do lokacji 1 lub 2", [1, 2]),
-    Location(1, "Obóz wojowników", "iść do obozu wojowników", None, "Jesteś w Obozie wojowników. Wokół siebie widzisz ludzi uzbrojonych po zęby rozmawiających na różne tematy. Możesz tutaj spokojnie odpocząć przed następną wyprawą w nieznane.", 1),
-    Location(2, "Miasto kupne", "iść do miasta kupnego", None, "Jesteś w mieście. Otaczają Cię ulice tętniące życiem, wydaje się niemal idealne. Kupce targują się, rodziny spacerują i życie się toczy. Jednak wiesz że coś jest nie tak, ciemne uliczki skrywające sekrety, plotki pełne kłamstw. Wpada Ci w ucho plotka o tak zwanym bazyliszku. Nigdy o czymś takim nie słyszałeś.", 1),
-    Location(3, "Dom rzeźbiarza", "iść do domu rzeźbiarza", None, "Wychodzisz do pustego domu rzeźbiarza...", 1),
-    Location(4, "Arena", "iść na arenę", None, "Jesteś na arenie. Tutaj każdy może się zapisać i walczyć o śmierć i życie za chwałę i pieniądze. Widzowie krzyczą z podekscytowania z każdym ciosem, wiedząc że jeden z nich będzie śmiertelny. ", 1),
-    Location(5, "Karczma 7.5 drwali", "iść do karczmy drwali", None, "Wchodzisz do pobliskiej karczmy. Jest tu dość pusto, jedynie trio wieśniaków pijących w kącie oraz rolnik zapijający smutki przy ladie. W samym tyle znajdują się drzwi które wydają się jakby nie pasowały tutaj. ", 1),
-    Location(6, "Farma jakiegoś fajnego gościa", "iść na farmę", None, "--", 1),
-    Location(7, "Las tysiącletni", "iść do lasu", None, "Jesteś w tysiącletnim lesie. Wokół Ciebie są tylko gigantyczne drzewa, większe od niektórych wierzy. Słyszysz dalekie odgłosy życia w lesie, znasz to miejsce, jednak łatwo się tutaj zgubić. Trzymaj się drogi jeśli Ci życie miłe.", 1),
-    Location(8, "Ruiny starej wieży", "iść do ruin starej wieży", None, "Opis: W głebi lasu znajdujesz starą wieże. Słyszałeś o niej w legendach i plotkach od ludzi z obozu. Mówi się że spaliła ją chimera, ale to było setki lat temu. Wieża nie jest wysoka, górne poziomy są zawalone, jednak kilka poziomów nadal można zwiedzić. Uważaj na szkielety dawnych rycerzy.", 1),
-    Location(9, "Studnia bez końca", "iść do studni", None, "Opis : Przed tobą widzisz dużą studnie. Widać że od dawna nie była używana. Patrząc na nią bliżej, zauważasz wajchę. Ciągnąc za nią, pokazują się schody prowadzące w głębię studni. Mówi się że prowadzi do środka ziemi, ludzie którzy wędrowali w głebi wracają z poparzeniami gorszymi niż od smoka. Przygotuj się.", 1),
-    Location(10, "Labirynt ", "iść do labiryntu", None, "Jesteś w labiryncie. Sciany wokół Ciebie wydają się masywne, niezniszczalne. Na każdym rogu widzisz szczątki poprzednich nieszczęśników którzy tutaj trafili. Ciarki Cię przechodzą gdy przypominasz sobie co grasuje w tym labiryncie. Mówi się że minotaur jest niepokonany, gdy Cię zobaczy, jesteś martwy. Znajdź wyjście i przygotuj się na cokolwiek możesz tutaj napotkać.", 1),
-    Location(11, "Bagna ", "iść na bagna", None, "Opis : Jesteś na bagnach. Gęsta mgła sprawia że trudno cokolwiek zobaczyć, a błoto spowalnia każdy Twój ruch. Bagna to niebezpieczne miejsce, uważaj na czające się w wodzie gady. Gdzieś w głebi tych terenów mieszka wiedźma. ", 1),
-    Location(12, "Smocze góry", "iść do lasu", None, "Opis : Droga w góry jest długa i trudna, ciężki wiatr ze śniegiem ograniczają Twoją wizję. W tych górach żyją potężne stwory czychające w jaskiniach. Najbezpieczniejszą opcją jest trzymanie się ścieżki. Mówi się że na samym szczycie znajduje się gniazdo smoków. Nawet jedno jajo można sprzedać za fortunę a smocze szczątki można przerobić na potężne przedmioty.", 1),
-    Location(13, "Ruiny zamku Karad", "iść do lasu", None, "Opis : Przed tobą znajdują się ruiny zamku Karad. ", 1),
-    Location(14, "Karczma \"Mały Łowca", "iść do lasu", None, "Jesteś w tysiącletnim lesie. Wokół Ciebie są tylko gigantyczne drzewa, większe od niektórych wierzy. Słyszysz dalekie odgłosy życia w lesie, znasz to miejsce, jednak łatwo się tutaj zgubić. Trzymaj się drogi jeśli Ci życie miłe.", 1)
-]
+Monster(1,
+        28,
+        100,
+        40,
+        "Hydra",
+        "Czujesz drżenie ziemi jak by było wywoływane prze ruch ogromnej istoty . Po woli przed tobą zaczynają się pojawiać wężowate głowy jedna po drugiej.",
+        "Walka skończyła się szybko przez swoją ignorancje i nie wiedzę dorobiłeś  potworowi kilka dodatkowych głów a sam skończyłeś rozszarpany i pożarty.",
+        "Po ciężkiej walce udało ci się wygrać a potwór leż martwy trawiony przez jedyną broń skuteczną przeciw niemu - ogień."
+        "Ucieczka nie była zbyt trudna ze względu na masę i szybkość stwora, jedynym problemem był zasięg jego ataku.")
+
+Monster(0.5,
+        15,
+        75,
+        25,
+        "Czerwony duch",
+        "Widzisz biegnącego mężczyznę gonionego przez średniej wielkości potwora przypominającego prześwitującego czerwonego byka z ogniem buchającym z nozdrzy",
+        "Próbujesz odciągnąć byka jednak w momencie gdy próbujesz go złapiać za rogi przechodzi przez ciebie prąd.",
+        "Postanowiłeś pomóc kiedy  byk miał już zabić mężczyznę oblewasz go  za pomocą wiadra wody stojącego obok zmoczony duch zaczyna uciekać.",
+        "Ignorujesz potwora wiedząc iż nie atakuje ludzi z byle jakich powodów , ten człowiek musiał sobie na to zasłużyć")
+
+Monster(0.7,
+        35,
+        78,
+        40,
+        "Mantikora",
+        "Zaglądasz do jaskini w jej wnętrzu jest słychać lwi ryk kiedy próbujesz wyostrzyć wzrok obok twojej głowy przelatuje ogon skorpiona ",
+        "Walka była długa jesteś zmęczony  potwór dostrzega twoją słabość i wykorzystuje lecąc i przygniatająca twoje ciała po czym odgryza ci głowę.",
+        "Walka była długa jesteś zmęczony ale tego nie pokazujesz , widząc swój koniec mantikora wbija swój ogon w kark i po umiera",
+        "zacząłeś uciekać  , jesteś już przy samym wyjściu nagle czujesz bul w okolicy żołądka patrzysz w dół a tam wystaje z ciebie kolec skorpiona.",)
+
+Monster(1.5,
+        14,
+        30,
+        5,
+        "Szkielet",
+        "Idziesz sobie spokojnie kiedy nagle zaczął wiać wiatr który zaczął ujawniać ludzkie kości . Zaczynasz się im przyglądać ,próbujesz podnosisz jedną z czaszek jak tylko zbliżasz rękę ta próbuję cię ugryźć cofasz się o krok po czym  czujesz coś na swoim ramieniu ,pomału się odwracasz a tam stoi armia szkieletów.",
+        " Nie mogąc wygrać z ich liczebnością zaczynasz uciekać będąc gonionym przez armię szkieletów .",
+        "Walka nie trwała długo ze względu na kruchość szkieletów szybko je rozpraszasz po czym miażdżysz ich czaszki by już nigdy nie wstali",
+        "Widząc liczbę nie umarłych próbujesz się wycofać , widząc lukę w ich ustawieniu biegniesz przed siebie ale niestety gubisz kilka przedmiotów.")
+
+Monster(0.9,
+        45,
+        25,
+        30,
+    "Bazyliszek",
+
+)
