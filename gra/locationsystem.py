2ef942be70a9f5303d4f6daf845b34abd0604dc2
tomash02 <70844011+tomash02@users.noreply.github.com>
2020-10-04 11:22:21 +0200

    UwUOwO
    jakieś zmiany

--- gra/locationsystem.py
+++ gra/locationsystem.py
@@ -37,7 +37,21 @@ def move(game_mgr: GameManager, droga):
         is_alive = game_mgr.player.on_monster_meet(currloc.monster)
         if is_alive == True:
             print("Brawo!!! Pokonałeś potwora \n" + currloc.go_msg)
+            possible_locations()
         else:
             from gra.ui import quit_game
 
             quit_game(game_mgr)
+
+def possible_locations():
+
+    from gra.config import locations
+
+    curr_possibs = get_curr_location().id_dst
+    for i in curr_possibs:
+        if locations[i].id == curr_possibs:
+            print(i + ")" + locations[i].go_msg)
+
+def cant_go(droga):
+    if droga not in get_curr_location().id_dst:
+        print("To miejsce jest za daleko")
