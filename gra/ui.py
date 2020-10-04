68a10a1de55fa5046cd39bbd448f1fbcd0e30b42
PW <53907986+wps0@users.noreply.github.com>
2020-10-04 11:25:18 +0200

    ui dst not reachable wip

--- gra/ui.py
+++ gra/ui.py
@@ -1,5 +1,5 @@
 from gra.game import GameManager
-from gra.locationsystem import move
+from gra.locationsystem import move, get_curr_location
 
 
 def quit_game(game_mgr: GameManager):
@@ -23,17 +23,22 @@ def game_info(game_mgr):
     print("---------------------------")
 
 
+def game_pwd(game_mgr):
+    print("Jesteś teraz w", get_curr_location().name)
+
 
 cmds = {
     "quit": quit_game,
     "help": game_help,
-    "info": game_info
+    "info": game_info,
+    "pwd": game_pwd()
 }
 
 cmds_desc = {
     "help": "Wyświetla okienko pomocy",
     "quit": "Zamyka grę",
     "info": "Wyświetla informacje o graczu",
+    "pwd": "Wyświetla obecną lokalizację"
 }
 
 running: bool = True
@@ -46,10 +51,17 @@ def main_loop(game_mgr):
         if cmd in cmds:
             cmds[cmd](game_mgr)
         elif cmd.isnumeric():
+            cmd = int(cmd)
+
+            if cmd not in get_curr_location().id_dst:
+                print("To miejsce jest za daleko!")
+                # TODO: print ten teges
+                continue
+
             import random as r
             missing_hp = game_mgr.player.base_hp - game_mgr.player.curr_hp
             game_mgr.player.regenerate_hp(r.randint(0, missing_hp // 2))
-            move(game_mgr, int(cmd))
-            print("Move to", cmd)
+            move(game_mgr, cmd)
+            # print("Move to", cmd)
         else:
             print("Command not found. Use 'help' to show help.")
